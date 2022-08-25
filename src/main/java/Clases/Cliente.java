66c0ac0545fd85562f880c402d116a722a61bdc6
308 <308@academicos.cesde.edu.co>
2022-08-25 10:28:53 -0500

    Se crean Clases cliente y producto

---
+++ src/main/java/Clases/Cliente.java
@@ -0,0 +1,24 @@
+package Clases;
+
+public class Cliente {
+
+    // ATRIBUTOS
+
+    public  String nombre;
+    public String cedula;
+    public byte edad;
+    public String celular;
+    public boolean aplicaDescuento;
+
+    // CONTRUCTOR
+
+    public Cliente() {
+    }
+
+    // METODO
+
+    public String saludar(){
+        return "oe";
+    }
+
+}
